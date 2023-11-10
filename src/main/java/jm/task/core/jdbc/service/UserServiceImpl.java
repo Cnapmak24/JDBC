a7c2212a56eab79afe8dda1fc56cbed04be90587
Cnapmak24 <you@example.com>
2023-11-10 14:45:22 +0500

    Исправлена ошибка возвращения null и исправлена зависимость на интерфейс UserDao

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
@@ -7,35 +8,29 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
-//    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    private final UserDao userDao = new UserDaoHibernateImpl();
+//    private final UserDao userDao = new UserDaoJDBCImpl();
     public void createUsersTable() {
-//        userDaoJDBC.createUsersTable();
-        userDaoHibernate.createUsersTable();
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-//        userDaoJDBC.dropUsersTable();
-        userDaoHibernate.dropUsersTable();
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-//        userDaoJDBC.saveUser(name, lastName, age);
-        userDaoHibernate.saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-//        userDaoJDBC.removeUserById(id);
-        userDaoHibernate.removeUserById(id);
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-//        return userDaoJDBC.getAllUsers();
-        return userDaoHibernate.getAllUsers();
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-//        userDaoJDBC.cleanUsersTable();
-        userDaoHibernate.cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 }
